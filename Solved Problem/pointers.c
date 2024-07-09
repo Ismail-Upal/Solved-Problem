7c052dc7eb0ffddd01f25a423ebf1df3a2dc9ae7
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2024-07-09 21:08:33 +0600

    added succesfully solved random problems

--- Solved Problem/pointers.c
+++ Solved Problem/pointers.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-int print(int n)  
+void print(int n)  
 {
     scanf("%d", &n);
     for(int i=1; i<=n; i++){
         printf("%d ", i);
     }
+
 }
 int main()
 {
     int a;
     print(a);
+    return 0;
 }
