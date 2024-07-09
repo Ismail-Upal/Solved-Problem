85051802ac5367a982eb9574a6ae2cfa1071d142
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2024-07-09 21:09:59 +0600

    added succesfully solved random problems

---
+++ Solved Problem/conditional2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if(tk>=100){
+        printf("Burger khabo");
+    }
+    else if(tk>=50){
+        printf("fuchka khab0");
+    }
+    else{
+        printf("khabo na");
+    }
+    return 0;
+}
